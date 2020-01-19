d764e1ace5bad19041f79299025988e258c3ce95
tojatos <tojatos@gmail.com>
2020-01-20 00:46:39 +0100

    lista13 - pair

--- lista13.scala
+++ lista13.scala
@@ -4,17 +4,26 @@ object Main {
 
   def main(args: Array[String]): Unit =
   {
-    zad5()
+    zad1()
+  }
+  def zad1() = {
+    val a = new Pair(5, "Ala")
+    println(a)
   }
   def zad5() = {
     def wordCounter(text: String): Map[String, Int] = {
       Map() ++ text.split(' ').groupBy(i => i).view.mapValues(_.size)
     }
     println(wordCounter("Ala ma kota Ala hej ho hej ho hej sokoły"))
-
   }
 }
 
-class Point(var x:Double = 0.0, var y:Double = 0.0) {
+class Pair[A,B](var fst: A, var snd: B)
+{
+  override def toString = s"($fst, $snd)"
+}
+
+class Point(var x: Double = 0.0, var y: Double = 0.0) {
   override def toString = "[" + x + ", " + y + "]"
 }
+//class Circle(override var x: Double = 0.0, override var y: Double = 0.0) extends Point

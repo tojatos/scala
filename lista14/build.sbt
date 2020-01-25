30df1640a0427cc00a4385f8c3b28b5e81e2e21a
tojatos <tojatos@gmail.com>
2020-01-25 19:36:22 +0100

    Lista14 - zad2 done

---
+++ lista14/build.sbt
@@ -0,0 +1,4 @@
+name := "Lista14"
+version := "1.0"
+scalaVersion := "2.13.1"
+libraryDependencies += "org.scala-lang.modules" %% "scala-swing" % "2.1.1"
---
+++ lista14/src/main/scala/main.scala
@@ -0,0 +1,27 @@
+import scala.swing._
+import scala.swing.event._
+
+object TextFields extends SimpleSwingApplication {
+  def top: Frame = new MainFrame {
+    title = "Text Fields"
+    size = new Dimension(200, 100)
+    preferredSize = size
+    resizable = false
+    val inText = new TextField(10)
+    val outText = new TextField(10) { editable = false }
+    val inLabel = new Label(" input")
+    val outLabel = new Label("output")
+    contents = new FlowPanel() {
+      contents += inLabel
+      contents += inText
+      contents += outLabel
+      contents += outText
+    }
+    listenTo(inText)
+    reactions += {
+      case EditDone(_) =>
+        outText.text = inText.text
+        inText.text = ""
+    }
+  }
+}
